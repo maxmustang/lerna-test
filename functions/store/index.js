10d2bb7b266b0098db62dfcbdc1e80705cc1bf71
Max Mattos <antonio.max@amedigital.com>
2020-04-05 13:18:09 -0300

    more tests with lerna

--- functions/store/index.js
+++ functions/store/index.js
@@ -1,3 +1,8 @@
-const fakeDb = require('lib-do-max')
+const {fakeDb, theChosenToDie} = require('lib-do-max')
 
-fakeDb
+fakeDb.listAll().then(x => {
+    const chosen = theChosenToDie(x.length)
+    return fakeDb.findInOrangeById(chosen)
+}).then(x => {
+    console.log(`The destiny chooos ${x.name} the ${x.class} to DIEEEEE` )
+})
